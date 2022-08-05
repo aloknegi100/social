23da37213e3686c96f46b0a08adeec6c4f9a46bd
Alok Singh Negi <aloknegi100@gmail.com>
2022-08-06 03:37:35 +0530

    finally temp merged

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const router=express.Router();
+const homeController=require('../controllers/home_controller');
+
+router.get('/',homeController.home);
+router.use('/post',require('./post'))
+router.use('/users',require('./users'));
+console.log("router loaded");
 
 
-
-console.log("router loaded")
 module.exports=router
 
