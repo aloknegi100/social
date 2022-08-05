7ac67af97903af6b793d447216b7697f486efdc9
Alok Singh Negi <aloknegi100@gmail.com>
2022-08-05 20:34:52 +0530

    index committed

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express();
+const port=8000;
+
+app.listen(port,(err)=>{
+    if(err)
+    {
+        console.log(`error in running the server: ${err} `)
+        return;
+    }
+    console.log(`port running successfully on port : ${port}`);
+})
