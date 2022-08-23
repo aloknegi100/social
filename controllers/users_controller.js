db9ef301d96ed08f7bcb19d1db69ece239225136
Alok Singh Negi <aloknegi100@gmail.com>
2022-08-23 16:09:42 +0530

    signup created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,55 @@
+const User=require('../models/user')
+
 module.exports.profile=function(req,res){
     res.write('<h1>User profile</h1>');
     res.end();
+}
+
+
+module.exports.signUp=function(req,res){
+    res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+
+}
+
+module.exports.signIn=function(req,res){
+    res.render('user_sign_in.ejs',{
+        title:"Codeial | Sign In"
+    })
+
+}
+module.exports.create=function(req,res){
+    if(req.body.password!=req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err)
+        {
+            console.log("error in signing up the user");
+            return res.redirect('back');
+        }
+        if(!user)
+        {
+            User.create(req.body,(err,user)=>{
+                if(err)
+                {
+                   console.log("error in creating the user for sign up"); 
+                   return res.redirect('back');
+                }
+                return res.redirect('/users/sign-in');
+
+            })
+        }
+        else{
+            return res.redirect('back');
+        }
+
+    })
+}
+
+
+module.exports.createSession=function(req,res){
+
 }
