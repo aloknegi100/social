f0a72c025b48a88212872b5998b3bf8c0e9681c7
Alok Singh Negi <aloknegi100@gmail.com>
2022-08-24 17:44:52 +0530

    .

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -67,27 +67,5 @@ module.exports.create=function(req,res){
 
 
 module.exports.createSession=function(req,res){
-    User.findOne({email:req.body.email},function(err,user){
-        if(err)
-        {
-            console.log("error in signing in the user");
-            return ;
-        }
-        if(user)
-        {
-            if(user.password==req.body.password)
-            {
-                res.cookie('user_id',user.id);
-                return res.redirect('/users/profile');
-            }
-            else
-            {
-            return res.redirect('back');
-            }
-        }
-        else
-        {
-            return res.redirect('back');
-        }
-    })
+   return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const port = 8000;
 const cookieParser=require('cookie-parser');
 const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
 
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static('./assets'));
 app.use(expressLayouts);
-app.use('/', require('./routes/index'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name:'codeial',
+    secret:'blahsomething',
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:1000*60*100
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', require('./routes/index'));
+
 
 app.listen(port, (err) => {
     if (err) {
