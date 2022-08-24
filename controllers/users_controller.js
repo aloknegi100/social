bb937dfe9cc1248f6fbbc8db5a35384574feed02
Alok Singh Negi <aloknegi100@gmail.com>
2022-08-25 05:17:03 +0530

    .

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,22 +1,12 @@
 const User=require('../models/user')
 
 module.exports.profile=function(req,res){
-    if(req.cookies.user_id){
-    User.findById(req.cookies.user_id,function(err,user)
-    {
-        if(user)
-        {
+    
             return res.render('profile.ejs',{
                 title:"Codeial | profile",
-                user:user
-            });
-        }
-        return res.redirect('/users/sign-in');
-    })   
-}
-else{
-    return res.redirect('/users/sign-in');
-}
+            })
+    
+   
         
 }
 
